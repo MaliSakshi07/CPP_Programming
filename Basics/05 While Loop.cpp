68946371d47b9e0299ab1c0ae89aad77c1b7da57
MaliSakshi07 <sakshimali467@gmail.com>
2024-11-29 21:30:47 +0530

    Basics

---
+++ Basics/05 While Loop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+
+    while(i <=10)
+    {
+        cout<<"\nWel-come",i;
+        i++;
+    }
+
+    cout<<"\n\nThanks!!!";
+
+    getch();
+    return 0;
+}
