19f3ae3c8f738e66ce4e9f47dd000acb2d16f963
MaliSakshi07 <sakshimali467@gmail.com>
2024-11-30 22:30:12 +0530

    Basics

---
+++ Basics/06 dowhile.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int i=0;
+
+    do{
+        cout<<"\nWel-come",i;
+        i++;
+    }
+    while(i<10);
+
+    getch();
+    return 0;
+}
