5ca10424ae9a7856c14c8ab679cd7d2053aa9ace
MaliSakshi07 <sakshimali467@gmail.com>
2024-12-01 22:56:15 +0530

    Basics

---
+++ Basics/07 for loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int i=0;
+
+    for(i=0; i<10; i++)
+    {
+        cout<<"\nWel-Come";
+    }
+
+    getch();
+    return 0;
+}
