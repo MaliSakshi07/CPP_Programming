1d02fa0ceb847b9ba5a78a57aebc81d037459e4e
MaliSakshi07 <sakshimali467@gmail.com>
2024-12-25 21:47:10 +0530

    Inheritance

---
+++ Inheritance/02 Single Level - Inheritance.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter a number";
+    cin>>a;
+
+    c=1;
+    b=0;
+    for(int i=1;i<=10;i++)
+    {
+        c=(i*a);
+        b=c+i;
+
+    }
+     cout<<"\n"<<b;
+
+
+
+}
