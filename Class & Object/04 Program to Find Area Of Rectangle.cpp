73ebd6e3e29edacd2e7835d02374297baffe196e
MaliSakshi07 <sakshimali467@gmail.com>
2025-03-10 23:27:16 +0530

    Class & Object

---
+++ Class & Object/04 Program to Find Area Of Rectangle.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Rectangle
+{
+    public:
+        float Area = 0, length = 0, width = 0;
+        void RectArea();
+
+};
+
+void Rectangle :: RectArea()
+{
+    Area = length*width;
+}
+
+int main()
+{
+    Rectangle obj;
+    obj.length = 5;
+    obj.width = 3;
+
+    obj.RectArea();
+    cout<<"Area of Rectangle : "<<obj.Area<<endl;
+
+    getch();
+    return 0;
+}
